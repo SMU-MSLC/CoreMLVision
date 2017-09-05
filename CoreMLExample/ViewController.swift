f73db470bf942ab72af6080b70dd8d4c2c5f845e
Eric Larson <eclarson@eric.local>
2017-09-05 18:22:42 -0500

    Initial Commit

---
+++ CoreMLExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreMLExample
+//
+//  Created by Eric Larson on 9/5/17.
+//  Copyright © 2017 Eric Larson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
